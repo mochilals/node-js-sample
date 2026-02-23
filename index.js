c376e187d59ceb7baee28234a8fc5c863c0ce42d
Hilal <mochilalls@gmail.com>
2026-02-23 10:49:44 +0700

    new

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  console.error("🔥 ERROR:", err.stack);
+
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error"
+  });
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-// app.get('/error', (req, res) => {
-//   throw new Error("Simulated production crash");
-// });
+app.get('/error', (req, res) => {
+  throw new Error("Simulated production crash");
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
